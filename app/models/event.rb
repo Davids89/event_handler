beaf9b6fe485215d87ddbbab4e219e01ffe3c65c
David Luque <daviluqui@gmail.com>
2020-10-17 11:17:34 +0200

    Added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  validates :end_date, timeliness: {after: :start_date}
+  validates :title, presence: true
 end
