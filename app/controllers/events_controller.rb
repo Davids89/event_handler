6dcc27db4e43da5aaaf5a03bf3a7392316dc7125
David Luque <daviluqui@gmail.com>
2020-10-19 19:49:45 +0200

    Added strong parameters to show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,10 @@ class EventsController < ApplicationController
   include EventInteractor
 
   def show
-    render_show get_month_events(params[:id])
+    render_show get_month_events(params_show[:id])
+  end
+
+  def params_show
+    params.permit(:id)
   end
 end
