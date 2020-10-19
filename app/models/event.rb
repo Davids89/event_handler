228ebc6863c4ab8f93bda412f428854622997a9e
David Luque <daviluqui@gmail.com>
2020-10-19 19:40:46 +0200

    Event module method to get filtered events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,18 @@
 class Event < ApplicationRecord
+  include EventRepresentations
+
   validates :start_date, presence: true
   validates :end_date, presence: true
 
   validates :end_date, timeliness: {after: :start_date}
   validates :title, presence: true
+
+  scope :filter_by_month, ->(month) do
+    year = Time.zone.now.year
+
+    min_start_date = Time.new(year, month)
+    max_end_date = min_start_date.end_of_month
+
+    where(start_date: min_start_date..max_end_date, end_date: min_start_date..max_end_date)
+  end
 end
