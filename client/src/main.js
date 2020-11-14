faae601dcc54bf5f1e555f6c01d883c85420b831
David Luque <daviluqui@gmail.com>
2020-11-14 12:55:27 +0100

    Delete imports unnecesaries

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
 import i18n from './js/i18n'
 import router from './js/router'
-import VueI18n from 'vue-i18n'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 
@@ -9,7 +8,6 @@ import vueEventCalendar from 'vue-event-calendar'
 
 import './vue-resource-config'
 
-Vue.use(VueI18n)
 Vue.use(VueResource)
 Vue.use(vueEventCalendar, {locale: 'en'})
 
